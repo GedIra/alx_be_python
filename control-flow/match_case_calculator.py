58ecff7dc999dcd136706ae2c1eb651dc52269c4
GedIra <irankundag65@gmail.com>
2024-09-21 21:02:02 -0500

    handling the checkers error

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -2,7 +2,7 @@ num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
 operator = input("Choose the operation (+, -, *, /):")
 
-match operator:
+match operator :
 
     case '+':
         result = num1 + num2
