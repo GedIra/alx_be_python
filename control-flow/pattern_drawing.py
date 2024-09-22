67f17de9ec78f1e836acad826db7cf906557413f
GedIra <irankundag65@gmail.com>
2024-09-21 21:48:01 -0500

    Creation of pattern_drawing.py file

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,11 @@
+length = int(input("Enter the size of the pattern: "))
+row = 1
+
+while row <= length:
+    for char in range(0,length):
+        print("*", end="")
+    
+    else:
+        print("")
+
+    row += 1
