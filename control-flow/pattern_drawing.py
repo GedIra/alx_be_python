bdf3757324a1a322ef04889b8941a7bacf1446bf
GedIra <irankundag65@gmail.com>
2024-09-22 20:17:15 -0500

    3 Editting pattern_drawing2.py to help a freind

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -3,7 +3,7 @@ number = int(input("Enter the size of the pattern: "))
 c = 1
 
 while c <= number:
-    for i in range(number):
+    for i in range(number) :
         print("*", end="")
     print()
     c += 1
