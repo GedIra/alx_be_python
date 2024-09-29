898129489c5e2c94d0c504c1029b07a32b880fad
GedIra <irankundag65@gmail.com>
2024-09-29 18:36:47 +0200

    creation of main.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,35 @@
-#creation of arithmetic_operations.py file
+
+
+
+def perform_operation(operation, num1, num2): #function definition
+  #body
+
+  result = 'Invalid'
+   
+  match operation: #(+ * - /)
+
+    case 'add':
+      return num1 + num2
+      
+    
+    case 'subtract':
+      return num1 - num2
+
+    case 'multiply':
+      return num1 * num2
+    
+    case 'divide':
+
+      if num2 == 0:
+        print("You cannot divide by zero.")
+        return Null
+      
+      else:
+        return num1 / num2
+
+    case _:
+      print('Invalid operation')
+      return Null
+
+
+
