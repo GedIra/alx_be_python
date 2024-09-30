67821499a5a3f0e0cf3aa2f7e7dd9180de453a20
GedIra <irankundag65@gmail.com>
2024-09-30 21:38:40 +0200

    creation of explore_datetime.py file

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,25 @@
+import datetime as timepac
+
+def display_current_datetime():
+
+  date_now = timepac.datetime.now()
+
+  return date_now
+
+def calculate_future_date(days_to_be_added, current_date):
+  days_to_be_added_date = timepac.timedelta(days = days_to_be_added)
+  end_date = current_date + days_to_be_added_date
+
+  return end_date
+
+current_date = display_current_datetime()
+
+print('Current date and time: ' + current_date.strftime('%Y-%m-%d %X'))
+
+days = int(input("Enter the number of days to add to the current date: "))
+
+
+future_date = calculate_future_date(days, current_date)
+
+print(f'Future date: ' + future_date.strftime('%Y-%m-%d'))
+
